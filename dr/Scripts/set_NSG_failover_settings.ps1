d1a10640d4df4af3f6cd1a8a93a8e17a6c05bd06
Pedro Zuzi <pedropz0750@gmail.com>
2025-02-27 16:16:14 -0300

    update

--- dr/Scripts/set_NSG_failover_settings.ps1
+++ dr/Scripts/set_NSG_failover_settings.ps1
@@ -1,4 +1,10 @@
-Connect-AzAccount
+#Connect-AzAccount
+
+#variables
+$drNSGName = "secondary_nsg"
+$drNSGResourceGroupName = "rg-secondary"
+
+$drNSG = Get-AzNetworkSecurityGroup -Name $drNSGName -ResourceGroupName $drNSGResourceGroupName
 
 $vault = Get-AzRecoveryServicesVault -Name "secondary-rsv"
 Set-AzRecoveryServicesAsrVaultContext -Vault $vault | Out-Null
@@ -11,12 +17,29 @@ $replicatedItems = Get-AzRecoveryServicesAsrReplicationProtectedItem -Protection
 
 # Fetching the Protected Item Object (for the Protected VM)
 
-foreach ($item in $replicatedItems) {
-    $nic1 = New-AzRecoveryServicesAsrVMNicConfig -NicId $nicId `
-        -ReplicationProtectedItem $item `
-        -RecoveryVMNetworkId $item.NicDetailsList[0].RecoveryVMNetworkId `
-        -RecoveryNetworkSecurityGroupId "/subscriptions/d0eff89d-ff78-4c52-be45-e46370c50d94/resourceGroups/rg-secondary/providers/Microsoft.Network/networkSecurityGroups/secondary_nsg"
+if ($drNSG){
+    foreach ($item in $replicatedItems) {
+        if ($item.NicDetailsList.count -eq 1){
+            Write-Output "Changing NSG Failover configuration from: ($($item.FriendlyName))"
+            $nicId = $item.NicDetailsList[0].NicId
+            $nic1 = New-AzRecoveryServicesAsrVMNicConfig -NicId $nicId `
+                -ReplicationProtectedItem $item `
+                -RecoveryVMNetworkId $item.NicDetailsList[0].RecoveryVMNetworkId `
+                -RecoveryNetworkSecurityGroupId $drNSG.Id
+        
+            $nics = @($nic1)
+            $job = Set-AzRecoveryServicesAsrReplicationProtectedItem -InputObject $item -ASRVMNicConfiguration $nics
+            sleep 7
+            Write-Host "($($item.FriendlyName)) : " -NoNewline
+            Write-Host (Get-AzRecoveryServicesAsrJob -Job $job).State
 
-    $nics = @($nic1)
-    $job = Set-AzRecoveryServicesAsrReplicationProtectedItem -InputObject $item -ASRVMNicConfiguration $nics
+        } else {
+            Write-Error "VM has more than one NIC"
+            $item.NicDetailsList
+            exit
+        }
+    }
+} else {
+    Write-Error "NSG not found"
+    exit
 }
